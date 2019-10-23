b7537560617127f731cccefe1212105f1e2464a6
DepInjoy <dep_injoy@163.com>
2019-10-23 13:54:15 +0800

    设置jupyter默认工作路径

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -211,7 +211,7 @@
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-#c.NotebookApp.notebook_dir = ''
+c.NotebookApp.notebook_dir = './jupyters'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
